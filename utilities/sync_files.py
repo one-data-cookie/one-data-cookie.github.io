9e3990781689a1923ff1a69d52b611e8656f250c
one-data-cookie <kolacek.m@gmail.com>
2021-12-31 13:05:10 +0100

    update path

--- utilities/sync_files.py
+++ utilities/sync_files.py
@@ -15,11 +15,11 @@ categories = [
 
 # Sync notes
 for cat in categories:
-    source_notes = '/Users/michal/GitHub/digi-garden/_notes/' + cat
+    source_notes = '/Users/michal/GitHub/one-data-cookie.github.io/_notes/' + cat
     target_notes = '/Volumes/GoogleDrive/My Drive/Obsidian/3. Resources/' + cat.replace('-', ' ').title()
     sync(source_notes, target_notes, 'sync', purge=True, create=True)
 
 # Sync files
-source_files = '/Users/michal/GitHub/digi-garden/assets/src'
+source_files = '/Users/michal/GitHub/one-data-cookie.github.io/assets/src'
 target_files = '/Volumes/GoogleDrive/My Drive/Obsidian/_resources'
 sync(source_files, target_files, 'sync')

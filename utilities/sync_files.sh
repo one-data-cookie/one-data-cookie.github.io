d932c6c12f86593cacbb03998f69f38e278f00bc
one-data-cookie <kolacek.m@gmail.com>
2022-01-05 00:08:53 +0100

    change files copying to replacing

--- utilities/sync_files.sh
+++ utilities/sync_files.sh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 
-# copy files from vault to assets
+# replace files from vault in assets
+# https://unix.stackexchange.com/questions/412259/how-can-i-copy-a-directory-and-rename-it-in-the-same-command
+rm -r assets/files
 cp -R _notes/__files/. assets/files
 
 # change file links to assets
+# https://www.grymoire.com/Unix/Sed.html
 find _notes -type f -name "*.md" -not -path "_notes/_*" -exec sed -i '' -e "s#\.\./__files/#\.\./\.\./assets/files/#g" {} \;
 
 # sync notes

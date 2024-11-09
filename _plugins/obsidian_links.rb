500fbc1dce60388931120d2c81a7b07aa8747747
one-data-cookie <kolacek.m@gmail.com>
2024-11-09 14:04:47 +0100

    Remove lowercasing of titles in links

--- _plugins/obsidian_links.rb
+++ _plugins/obsidian_links.rb
@@ -2,7 +2,7 @@ Jekyll::Hooks.register :documents, :pre_render do |document|
   document.content.gsub!(/\[\[(.*?)\|(.*?)\]\]/) do
     title = Regexp.last_match(1).strip
     label = Regexp.last_match(2).strip
-    title_url = title.downcase.gsub(/\s+/, '-')
+    title_url = title.gsub(/\s+/, '-')
     "[#{label}](#{title_url})"
   end
 end
